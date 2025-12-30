10dc76b6a9fbefb4b402b37a03c26179abdcc2ba
Sunghwan Jung <67978086+omega3jung@users.noreply.github.com>
2025-12-30 18:58:52 +0900

    fix(ui): to follow PascalCase, remove custom component

--- src/components/custom/AvatarMultiCombobox/index.ts
+++
@@ -1,5 +0,0 @@
-// src/components/custom/AvatarMultiComboBox/index.ts
-
-export { AvatarMultiComboBox } from "./AvatarMultiComboBox";
-export type { AvatarMultiComboBoxProps } from "./types";
-export { comboBoxVariants } from "./variants";
